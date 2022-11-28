19ffa32066196d9df1e0cb9205b4e3e3c0b3b587
Asushiny <1204693790@qq.com>
2022-11-28 10:14:42 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
-git config --global http.sslVerify "false"
-git add .
-git commit  -m  "deploy"
-git push -u origin main
+# 确保脚本抛出遇到的错误
+set -e
+
+git init
+git add -A
+git commit -m 'deploy'
+ 
+git push -f "git@github.com:Asushiny/Asushiny.github.io.git" main
+cd -
